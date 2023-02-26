ad331184e0ef841f9b4d4e56a3083c4026270bdd
Flor <fayelenramon@gmail.com>
2023-02-26 19:36:48 -0300

    lista de items

---
+++ src/app/mock-task.ts
@@ -0,0 +1,27 @@
+import { Task } from "./Task"
+export const TASKS: Task[]=[
+    {
+        id:1,
+        text:"terminar primer modulo de Angular",
+        day: 'Marzo 1 a las 12:00',
+        reminder: true
+    },
+    {
+        id:2,
+        text:"Estudiar",
+        day: 'Marzo 1 a las 14:00',
+        reminder: true
+    },
+    {
+        id:3,
+        text:"Leer",
+        day: 'Marzo 1 a las 16:00',
+        reminder: false
+    },
+    {
+        id:4,
+        text:"Jugar",
+        day: 'Marzo 1 a las 19:00',
+        reminder: true
+    },
+]
